7a910f6a0447d4417f36c6f87d2447306527432f
egarciateco <131554785+egarciateco@users.noreply.github.com>
2026-04-19 23:33:05 -0300

    Add new screens to the navigation stack

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-// App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import ProductListScreen from './src/screens/ProductListScreen';
+import ComparisonScreen from './src/screens/ComparisonScreen';
+import SubscriptionScreen from './src/screens/SubscriptionScreen';
 
 const Stack = createStackNavigator();
 
@@ -15,8 +17,11 @@ const App = () => {
           headerShown: false,
         }}
       >
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Productos" component={ProductListScreen} />
+        <Stack.Screen name="ProductList" component={ProductListScreen} />
+        <Stack.Screen name="Comparison" component={ComparisonScreen} />
+        <Stack.Screen name="Subscription" component={SubscriptionScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
